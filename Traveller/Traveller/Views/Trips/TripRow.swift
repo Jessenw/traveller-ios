4d6ce26254e81af4b27b927ef0e23eb5d9124be9
Jesse Williams <jessenw3@gmail.com>
2024-06-24 23:58:14 +1200

    tra-2: ensure consisten TripRow sizing

--- Traveller/Traveller/Views/Trips/TripRow.swift
+++ Traveller/Traveller/Views/Trips/TripRow.swift
@@ -15,36 +15,37 @@ struct TripRow: View {
             // Trip Name
             Text(trip.name)
                 .font(.headline)
-                .padding(.top, 8)
             
             // Start/end dates
-            HStack {
-                Image(systemName: "calendar")
-                    .foregroundColor(.gray)
-                if let startDate = trip.startDate, let endDate = trip.endDate {
+            if let startDate = trip.startDate, let endDate = trip.endDate {
+                HStack {
+                    Image(systemName: "calendar")
+                        .foregroundColor(.gray)
                     Text("\(startDate, formatter: dateFormatter) - \(endDate, formatter: dateFormatter)")
                         .font(.subheadline)
                         .foregroundColor(.gray)
                 }
             }
-            .padding(.bottom, 16)
             
             // Avatars
             let members = trip.members
-            HStack(spacing: 8) {
-                ForEach(members.prefix(5), id: \.self) { color in
-                    Circle()
-                        .fill(color)
-                        .frame(width: 30, height: 30)
+            if !members.isEmpty {
+                HStack(spacing: 8) {
+                    ForEach(members.prefix(5), id: \.self) { color in
+                        Circle()
+                            .fill(color)
+                            .frame(width: 30, height: 30)
+                    }
+                    if members.count > 5 {
+                        Text("+ \(members.count - 5) more")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
                 }
-                if members.count > 5 {
-                    Text("+ \(members.count - 5) more")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
+                .padding(.top, 16)
             }
-            .padding(.bottom, 8)
         }
+        .padding(.vertical, 8)
         .padding(.horizontal)
     }
 }
