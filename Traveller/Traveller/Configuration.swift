c59e1055d3f71ca6664969be9a9b5fec7dbe88ea
Jesse Williams <jessenw3@gmail.com>
2024-06-25 17:27:46 +1200

    tra-7: add property for GMS services API key

---
+++ Traveller/Traveller/Configuration.swift
@@ -0,0 +1,12 @@
+//
+//  Configuration.swift
+//  Traveller
+//
+//  Created by Jesse Williams on 25/06/2024.
+//
+
+import Foundation
+
+struct Configuration {
+    static let GMSServicesAPIKey = "<Insert key here>"
+}
