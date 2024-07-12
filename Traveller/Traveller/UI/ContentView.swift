354da25aa8be13b2776fae98eacaf993cd279c59
Jesse Williams <jessenw3@gmail.com>
2024-07-12 17:30:35 +1200

    sheet conversion

--- Traveller/Traveller/UI/ContentView.swift
+++ Traveller/Traveller/UI/ContentView.swift
@@ -2,17 +2,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ResizableAnchoredShape()
+        ResizableAnchoredShapeSheet {
+            HStack {
+                Text("Header")
+                    .font(.title)
+                Spacer()
+                Button(
+                    action: {},
+                    label: {
+                        Image(systemName: "multiply.circle.fill")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                    }
+                )
+                .buttonStyle(PlainButtonStyle())
+            }
+        } content: {
+            VStack {
+                ForEach(0..<5) { i in
+                    HStack {
+                        Text("\(i)")
+                        Spacer()
+                    }
+                }
+            }
+        }
     }
 }
     
 #Preview {
     ContentView()
 }
+
+struct HeaderSizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
+
+struct ContentSizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
     
-struct ResizableAnchoredShape: View {
+struct ResizableAnchoredShapeSheet<Header: View, Content: View>: View {
     @State private var scale: CGFloat = 1.0
     @State private var anchor: UnitPoint = .top
+    
+    let header: Header?
+    let content: Content?
+    
+    @State private var headerSize: CGSize = .zero
+    @State private var contentSize: CGSize = .zero
+    private let cornerRadius: CGFloat = 40
+    
+    init(
+        @ViewBuilder header: () -> Header?,
+        @ViewBuilder content: () -> Content?
+    ) {
+        self.header = header()
+        self.content = content()
+    }
 
     var body: some View {
         GeometryReader { geometry in
@@ -23,6 +72,47 @@ struct ResizableAnchoredShape: View {
                     height: geometry.size.height * scale
                 )
                 .offset(y: calculateOffset(geometry: geometry))
+                .overlay {
+                    VStack {
+                        // MARK: - Header
+                        if let header {
+                            header
+                                .padding()
+                                .background(
+                                    GeometryReader { geo in
+                                        Color.clear
+                                            .preference(key: HeaderSizePreferenceKey.self, value: geo.size)
+                                    }
+                                        .border(.red, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                                )
+                                .onPreferenceChange(HeaderSizePreferenceKey.self, perform: { size in
+                                    let _ = print("Header size: \(size)")
+                                    headerSize = size
+                                })
+                        }
+                        
+                        // MARK: - Content
+                        if let content {
+                            content
+                                .padding()
+                                .background(
+                                    GeometryReader { geo2 in
+                                        Color.clear
+                                            .preference(key: ContentSizePreferenceKey.self, value: geo2.size)
+                                    }
+                                        .border(.green, width: 3)
+                                )
+                                .onPreferenceChange(ContentSizePreferenceKey.self, perform: { size in
+                                    let _ = print("Content size: \(size)")
+                                    contentSize = size
+                                })
+                        }
+                        Spacer()
+                    }
+                    .frame(height: headerSize.height + contentSize.height)
+                    .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+                    .padding()
+                }
         }
         .frame(width: 200, height: 100) // Initial size
         .onTapGesture {
