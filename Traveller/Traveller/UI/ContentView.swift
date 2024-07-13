5433dbf372c1d8a3b62414547ef17d711b7e8d86
Jesse Williams <jessenw3@gmail.com>
2024-07-13 23:30:46 +1200

    omg it works

--- Traveller/Traveller/UI/ContentView.swift
+++ Traveller/Traveller/UI/ContentView.swift
@@ -35,7 +35,6 @@ struct ContentView: View {
 }
 
 struct MeasureSizeModifier<Key: PreferenceKey>: ViewModifier where Key.Value == CGSize {
-    
     let callback: (_ size: Key.Value) -> Void
     
     func body(content: Content) -> some View {
@@ -51,11 +50,6 @@ struct MeasureSizeModifier<Key: PreferenceKey>: ViewModifier where Key.Value == 
     }
 }
 
-struct HeaderSizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
-}
-
 struct ContentSizePreferenceKey: PreferenceKey {
     static var defaultValue: CGSize = .zero
     static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
@@ -69,9 +63,8 @@ struct ResizableAnchoredShapeSheet<Header: View, Content: View>: View {
     let header: Header?
     let content: Content?
     
-    @State private var headerSize: CGSize = .zero
-    @State private var contentSize: CGSize = .zero
-    @State private var currentHeight: CGFloat = .zero
+    @State private var originalSize: CGSize = .zero
+    @State private var currentSize: CGSize = .zero
     
     private let cornerRadius: CGFloat = 40
     
@@ -88,72 +81,53 @@ struct ResizableAnchoredShapeSheet<Header: View, Content: View>: View {
             ZStack(alignment: .top) {
                 RoundedRectangle(cornerRadius: 20)
                     .fill(Color.blue)
-                    .frame(height: currentHeight)
-
-                VStack {
-                    // MARK: - Header
-                    if let header {
-                        header
-                            .padding()
-                            .onAppear {
-                                currentHeight = headerSize.height
+                    .frame(height: currentSize.height)
+                    .overlay(alignment: .top) {
+                        VStack {
+                            // MARK: - Header
+                            if let header {
+                                header.padding()
                             }
-                            .modifier(MeasureSizeModifier<HeaderSizePreferenceKey> { size in
-                                headerSize = size
-                            })
-                    }
-                    
-                    // MARK: - Content
-                    if let content {
-                        if expanded {
-                            content
-                                .padding()
-                                .modifier(MeasureSizeModifier<ContentSizePreferenceKey> { size in
-                                    contentSize = size
-                                })
-                        } else {
-                            content
-                                .padding()
-                                .modifier(MeasureSizeModifier<ContentSizePreferenceKey> { size in
-                                    contentSize = size
-                                })
-                                .hidden()
+                            
+                            // MARK: - Content
+                            if let content, expanded {
+                                content.padding()
+                            }
                         }
+                        .modifier(MeasureSizeModifier<ContentSizePreferenceKey> { size in
+                            if currentSize == .zero {
+                                currentSize = size
+                            }
+                            withAnimation {
+                                currentSize = size
+                            }
+                        })
+                        .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
                     }
-                }
-                .clipShape(
-                    RoundedRectangle(cornerRadius: cornerRadius)
-                        
-                )
             }
             .offset(y: calculateOffset(geometry: geometry))
         }
-        .frame(width: 200, height: currentHeight) // Initial height
+        .frame(width: 200, height: currentSize.height) // Initial height
         .onTapGesture {
             withAnimation(.spring()) {
                 expanded.toggle()
                 
-                print("expanded: \(expanded)")
-                
                 if expanded {
-                    let originalScale = headerSize.height
-                    let targetScale = (headerSize.height + contentSize.height)
-                    let expandedScale = targetScale / originalScale
-                    scale = expandedScale
+                    scale = currentSize.height / originalSize.height
                     anchor = .top
                 } else {
                     scale = 1.0
                     anchor = .bottom
                 }
                 
-                currentHeight = headerSize.height * scale
+                originalSize = currentSize
             }
         }
     }
     
     private func calculateOffset(geometry: GeometryProxy) -> CGFloat {
         let originalHeight = geometry.size.height
-        let newHeight = currentHeight
+        let newHeight = currentSize.height
         let difference = newHeight - originalHeight
         
         switch anchor {
